cedacf5cac5eb7580eaeff3d17c09859115c9c6e
Renato Ramiro <renato.ramiro@virtus.ufcg.edu.br>
2018-05-10 15:06:12 -0300

    Fixing bug on component.module

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { ActivateUserComponent } from './activate-user/activate-user';
 import { CreateClientComponent } from './create-client/create-client';
 import { RegistrationComponent } from './registration/registration';
-import { SendCreditComponent } from './send-credit/send-credit';
 @NgModule({
 	declarations: [ActivateUserComponent,
     CreateClientComponent,
-    RegistrationComponent,
-    SendCreditComponent],
+    RegistrationComponent],
 	imports: [],
 	exports: [ActivateUserComponent,
     CreateClientComponent,
-    RegistrationComponent,
-    SendCreditComponent]
+    RegistrationComponent]
 })
 export class ComponentsModule {}
