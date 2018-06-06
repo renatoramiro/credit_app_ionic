98f5fb17b62b89658198a6d993dc531311f5648d
Renato Ramiro <renato.ramiro@virtus.ufcg.edu.br>
2018-06-06 16:39:36 -0300

    Implemented socket

---
+++ src/providers/socket/socket.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'phoenix-channels';
+
+/*
+  Generated class for the SocketProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SocketProvider {
+
+  private socket
+  private channel;
+
+  constructor() {
+    console.log('Hello SocketProvider Provider');
+  }
+
+  connect(token) {
+    this.socket = new Socket("ws://localhost:4000/socket", {params: {token: token}});
+    this.socket.connect();
+  }
+
+  getCurrentSocket() {
+    return this.socket;
+  }
+
+  join(socket, user_id) {
+    this.channel = socket.channel("room:" + user_id, {});
+    this.channel.join()
+          .receive("ok", resp => { console.log("Joined successfully", resp) })
+          .receive("error", resp => { console.log("Unable to join", resp) });
+  }
+
+  getCurrentChannel() {
+    return this.channel;
+  }
+
+}
